f19cfc956533b704ef6b778459c148c4cb6967a4
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-09-11 21:01:32 +0930

    fix: resolve merge conflicts

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_VERSION = 'v1';
+const STATIC_CACHE = `static-${CACHE_VERSION}`;
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== STATIC_CACHE).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  const assetRegex = /\.(?:html|js|css|png|jpg|jpeg|gif|svg)$/;
+  if (assetRegex.test(url.pathname)) {
+    event.respondWith(
+      caches.match(event.request).then(res => {
+        return (
+          res ||
+          fetch(event.request).then(response => {
+            const copy = response.clone();
+            caches.open(STATIC_CACHE).then(cache => cache.put(event.request, copy));
+            return response;
+          })
+        );
+      })
+    );
+  }
+});
+
